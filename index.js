da314964a3601ce28e7023770e0875b0f8084dbd
cop1fab <bienaimecopain@gmail.com>
2020-01-28 16:31:30 +0200

    add an express-graphql server

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 import express from 'express';
-import dotenv from 'dotenv';
+import cors from 'cors';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
+
 
 const app = express();
 
+const schema =  buildSchema(`
+  type Query{
+      message: String
+  }
+`);
+
+const root = {
+    message: () => 'Welcome to Authors Haven'
+}
+
 const port = process.env.PORT || 8090
 
+
+app.use(cors());
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true,
+    pretty: true
+}));
 app.listen(port, () => {
     console.log(`server started, listening on port ${port}`);
 })
